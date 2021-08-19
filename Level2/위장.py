cd660788b4c19b3575d76e77fd2c0a1fda67e4e4
kwhn <kwhn9486@naver.com>
2021-08-20 04:40:00 +0900

    solved

--- Level2/위장.py
+++ Level2/위장.py
@@ -1,7 +1,9 @@
 clothes = [["crowmask", "headgear"], ["bluesunglasses", "mask"],
            ["smoky_makeup", "headgear"], ["a", "mask"]]
 
-def solution(clothes):
+def solution(clothes): #파스칼 삼각형 성질 이용
+    # 2C1 + 2C0 = 3C1 (안고르는 경우 + 2개중 하나 고르는 경우) - 종류 당
+    # 3C1 * 3C1 - 1(아무것도 안 고르는 경우)
     how = {}
     lis = []
     for i in clothes:
