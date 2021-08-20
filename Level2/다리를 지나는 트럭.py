4519951fc9ab9fd3a417d88e20a48933a45b795b
kwhn <kwhn9486@naver.com>
2021-08-21 00:15:24 +0900

    solved

--- Level2/다리를 지나는 트럭.py
+++ Level2/다리를 지나는 트럭.py
@@ -4,11 +4,11 @@ truck_weights = [7, 4, 5, 6]
 
 
 def solution(bridge_length, weight, truck_weights):
-    bridge = [0] * bridge_length
+    bridge = [0] * bridge_length #가상의 다리를 만듬
     count = 0
 
     while bridge:
-        count += 1
+        count += 1 #시간은 계속 흘러가고, 그럴때마다 왼쪽으로 한칸씩 땡겨주는 작업을 해준다
         bridge.pop(0)
         if truck_weights:
             if sum(bridge) + truck_weights[0] <= weight:
