727759960338a6045245910887b7317799d49918
kwhn <kwhn9486@naver.com>
2021-08-21 00:14:43 +0900

    solved

--- Level2/다리를 지나는 트럭.py
+++ Level2/다리를 지나는 트럭.py
@@ -4,23 +4,18 @@ truck_weights = [7, 4, 5, 6]
 
 
 def solution(bridge_length, weight, truck_weights):
-    li = []
-    su = []
+    bridge = [0] * bridge_length
     count = 0
 
-
-
-
-did = solution(bridge_length, weight, truck_weights)
-print(did)
-
-
-
-
-
-
-
-
+    while bridge:
+        count += 1
+        bridge.pop(0)
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
+    return count
 
 did = solution(bridge_length, weight, truck_weights)
 print(did)
