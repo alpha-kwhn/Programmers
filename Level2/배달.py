ab69d596b373700cda810e614e5be42484e7ad23
김응관 <kwhn9486@naver.com>
2023-12-24 22:21:49 +0900

    solved

--- Level2/배달.py
+++ Level2/배달.py
@@ -1,18 +1,22 @@
-N = 5
-road = [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]]
-K = 3
-
-
 def solution(N, road, K):
-    each = [0] * N
-    total = [each] * N
-    print(total)
+    vilage = [[1e8] * N for _ in range(N)]
+    answer = 1
 
-    for i in range(len(road)):
-        print(total[road[i][0] - 1][road[i][1] - 1])
-        print(road[i][2])
-        total[road[i][0] - 1][road[i][1] - 1] = road[i][2]
-        print(total)
+    for idx in road:
+        if vilage[idx[0] - 1][idx[1] - 1] == 0 and vilage[idx[1] - 1][idx[0] - 1] == 0:
+            vilage[idx[0] - 1][idx[1] - 1] = idx[2]
+            vilage[idx[1] - 1][idx[0] - 1] = idx[2]
+        elif vilage[idx[0] - 1][idx[1] - 1] > 0 and vilage[idx[1] - 1][idx[0] - 1] > 0:
+            vilage[idx[0] - 1][idx[1] - 1] = min(vilage[idx[0] - 1][idx[1] - 1], idx[2])
+            vilage[idx[1] - 1][idx[0] - 1] = min(vilage[idx[0] - 1][idx[1] - 1], idx[2])
 
-print(solution(N, road, K))
+    for path in range(N):
+        for start in range(N):
+            for end in range(N):
+                vilage[start][end] = min(vilage[start][end], vilage[start][path] + vilage[path][end])
 
+    for idx in range(1, len(vilage[0])):
+        if 0 < vilage[0][idx] <= K:
+            answer += 1
+
+    return answer
