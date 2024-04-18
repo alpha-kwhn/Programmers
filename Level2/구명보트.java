e595ff54b69bad58662947aeb59db42e7619d8c0
Eunggwan Kim <40496065+alpha-kwhn@users.noreply.github.com>
2024-04-18 16:07:13 +0900

    solved
    최대 탑승인원이 2명임을 제대로 보지 못하고 풀어서 시간을 오래잡아먹은 문제 -> 문제를 꼼꼼히 보자

---
+++ Level2/구명보트.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int count = 0;
+        int idx = 0;
+        for(int i=people.length-1; i>=idx; i--) {
+            if(people[i] + people[idx] <= limit) 
+                idx++;
+            count++; 
+        }
+        return count;
+    }
+}
