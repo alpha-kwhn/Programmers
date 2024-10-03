250c2e9e3ca7df328beaa3e5c54e2b3820198623
Eunggwan Kim <40496065+alpha-kwhn@users.noreply.github.com>
2024-10-03 20:29:45 +0900

    Create 피보나치 수.java

---
+++ Level2/피보나치 수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[100001];
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++) 
+            dp[i] = ((dp[i-1] % 1234567) + (dp[i-2] % 1234567)) % 1234567;
+        
+        return dp[n];
+    }
+}
